0c982e97b70a06b25ef9e29475963394309c508f
ogfunkycold <ogfunkycold@gmail.com>
2024-09-11 16:50:23 -0300

    2nd commit, add formatter, lazygit and markdown-preview

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,22 @@
+-- NOTE: Lightweight yet powerful formatter plugin for Neovim
+-- Load the plugin with the keybinding '<leader>lg'
+--
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open lazy git' },
+  },
+}
